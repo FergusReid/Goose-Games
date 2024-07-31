0fd4b3bde5bc795b39bbcda73a0883d52a5a3d10
FergusReid <78882910+FergusReid@users.noreply.github.com>
2024-07-31 11:31:48 +0100

    Revert "Update abilities.cs"
    This reverts commit 82e3e3b67c81809bb3658cc5cfeb744b7a01ef72.

--- Assets/Scripts/abilities.cs
+++ Assets/Scripts/abilities.cs
@@ -32,7 +32,7 @@ public class abilities : MonoBehaviour
                 Vector2 mousePos = new Vector2(temp.x - Screen.width/2, temp.y - Screen.height/2);
                 Vector2 playerPos = transform.position;
                 aimDir = mousePos - playerPos;
-                Debug.Log("fireball    " + Screen.width + "   " + mousePos.ToString());
+                Debug.Log("fireball" + Screen.width + "   " + mousePos.ToString());
             }
             
             fireball.SetActive(true);
